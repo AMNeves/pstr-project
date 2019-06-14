0040113a9c929f28bfd4831c5911c3c4b9f4d741
AMNeves <ami.neves.prog@gmail.com>
2019-06-14 12:22:53 +0100

    spark part done

--- part2/spark-py/run.bat
+++ part2/spark-py/run.bat
@@ -1,2 +1,2 @@
-docker run --network=ps-net -p 8888:8888 -v C:/Users/amnev/Desktop/PS/pstr-project/part2/spark-py/logs:/data -v C:/Users/amnev/Desktop/PS/pstr-project/part2/spark-py/notebooks:/home/jovyan/work nunopreguica/ps1819
+docker run --network=ps-net -p 8888:8888 -v C:/Users/amnev/Desktop/FCT/PSTR/projects/part2/spark-py/logs:/data -v C:/Users/amnev/Desktop/FCT/PSTR/projects/part2/spark-py/notebooks:/home/jovyan/work nunopreguica/ps1819
 pause
